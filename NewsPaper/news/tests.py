f1fef001156f5a0f6fc8022cfbc054123a1bc6e3
KissOfDestiny <77636637+KissOfDestiny@users.noreply.github.com>
2021-03-30 21:42:56 +0300

    Delete NewsPaper directory

--- NewsPaper/news/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-from news.models import *
-
-Post.objects.create(title='title1_title', text='text1_text')
-Post.objects.create(title='Bad_word', text='text text bad_word text')
-Post.objects.create(title='title3_title', text='text3_text')
--- NewsPaper/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Postlist, PostDetail
-
-
-urlpatterns = [
-    path('', Postlist.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
-
-]
